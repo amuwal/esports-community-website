febe6f480502517c5cbedd4454b6fcc57badbd4b
amuwal <1mitccc@gmail.com>
2023-11-30 23:25:09 +0530

    Add past events section

--- src/pages/tournaments/index.js
+++ src/pages/tournaments/index.js
@@ -1,6 +1,7 @@
 import Footer from "../../shared/components/Footer/index.js";
 import Header from "../../shared/components/Header/index.js";
 import FeaturedSection from "./components/featured-section.js";
+import PastEventsSection from "./components/past-events.js";
 import TopSection from "./components/top-section.js";
 export default function Tournaments() {
   return (
@@ -8,6 +9,7 @@ export default function Tournaments() {
       <Header />
       <TopSection />
       <FeaturedSection />
+      <PastEventsSection />
       <Footer />
     </>
   );
