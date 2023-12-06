507bb88e823a0bbec0b07dc67f998f620066e9b5
amuwal <1mitccc@gmail.com>
2023-12-06 09:20:47 +0530

    Update template for top events

--- frontend/src/shared/components/Event/templates/top-event.js
+++ frontend/src/shared/components/Event/templates/top-event.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Event = styled.div`
   width: 100%;
@@ -16,18 +17,42 @@ export default function TopEvent({ event }) {
   return (
     <>
       <Event key={event._id}>
-        <div className="w-full h-[65%]">
+        <Link
+          to={
+            event.type === "ONETIME"
+              ? "/events/" + event._id
+              : "/events/regular/" + event._id
+          }
+          className="w-full h-[65%]"
+        >
           <img
             className="h-full w-full rounded-tr-[16px] rounded-tl-[16px] object-cover"
             src={event.titleImage}
             alt={event.title}
           />
-        </div>
-        <div className="p-[25px] h-[35%] flex flex-col items-start justify-center font-semibold text-white uppercase">
-          <a className="hover:text-blue-400" href={event.discord_channel_link}>
-            {event.title}
+        </Link>
+        <div className="flex justify-between items-center">
+          <div className="p-[25px] h-[35%] flex flex-col items-start justify-center font-semibold text-white uppercase">
+            <a
+              className="hover:text-blue-400"
+              href={event.discord_channel_link}
+            >
+              {event.title}
+            </a>
+            <div className="text-gray-300 text-sm">{event.frequency}</div>
+          </div>
+          <a
+            href={event.discord_channel_link || "https://discord.com"}
+            target="_blank"
+            rel="noreferrer"
+            className="cursor-pointer h-[35px] mr-[25px]"
+          >
+            <img
+              className=" bg-white h-full w-auto rounded-full p-[5px] rotate-[-30deg]"
+              src="/arrow.png"
+              alt="go"
+            />
           </a>
-          <div className="text-gray-300 text-sm">{event.frequency}</div>
         </div>
       </Event>
     </>
